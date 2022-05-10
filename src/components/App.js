e64197e564004da0a7b0df221db3fb3202228a8e
Dat <dathoang31082001@gmail.com>
2022-05-10 11:21:07 +0700

    initial project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import '../components/App.scss';
+import Login from './Auth/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Login />
+      </header>
+    </div>
+  );
+}
+
+export default App;
