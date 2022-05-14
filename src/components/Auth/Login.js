6c6014db24cac8c769135ddcc5ea5cead5eeb434
Dat <dathoang31082001@gmail.com>
2022-05-14 20:48:01 +0700

    update state login

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -4,6 +4,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+        }
+
+    }
+    handleUserName = (event) => {
+        this.setState({
+            username: event.target.value,
+        })
+    }
+    handlePassWord = (event) => {
+        this.setState({
+            password: event.target.value,
+        })
+    }
+    handleSubmit = () => {
+        console.log("username:", this.state.username, "password:", this.state.password)
+
+    }
     render() {
         return (
             <div className='Login'>
@@ -14,13 +36,15 @@ class Login extends Component {
                     <form>
                         <div className="mb-3">
                             <label for="exampleInputEmail1" className="form-label">Username</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Enter your username' />
+                            <input type="email" className="form-control" id="exampleInputEmail1" placeholder='Enter your username'
+                                onChange={(event) => this.handleUserName(event)} />
                         </div>
                         <div className="mb-3">
                             <label for="exampleInputPassword1" className="form-label">Password</label>
-                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Enter your password' />
+                            <input type="password" className="form-control" id="exampleInputPassword1" placeholder='Enter your password'
+                                onChange={(event) => this.handlePassWord(event)} />
                         </div>
-                        <button type="submit" className="btn btn-primary">Log in</button>
+                        <button type="submit" className="btn btn-primary" onClick={() => { this.handleSubmit() }}>Log in</button>
                     </form>
                     <div className="signWith">
                         <p>
@@ -43,5 +67,6 @@ class Login extends Component {
         );
     }
 }
+
 
 export default Login;
